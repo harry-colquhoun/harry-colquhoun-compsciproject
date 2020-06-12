2ae2dca0a95da456048ffd842c19aa0647fb1fbf
harry-colquhoun <66852176+harry-colquhoun@users.noreply.github.com>
2020-06-13 00:31:04 +0100

    I have made a start

--- main.ts
+++ main.ts
@@ -1,1 +1,26 @@
-
+namespace SpriteKind {
+    export const host = SpriteKind.create()
+}
+scene.setBackgroundColor(2)
+info.setScore(0)
+info.setLife(3)
+let Host = sprites.create(img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . f f 
+c c c c c d d d e e f c . f e f 
+. f d d d d d e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f e f 
+. . . f e f f e f e e e e f f . 
+. . . f e f f e f e e e e f . . 
+. . . f d b f d b f f e f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`, SpriteKind.Player)
+Host.setPosition(10, 50)
+game.showLongText("Welcome to Quiz night! How high can your score get with just 3 lives?", DialogLayout.Bottom)
